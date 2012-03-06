2f684053cb7dfb6e14eb5256c9e77153c7ac1a1c
SystemTOGA <gentleman@SystemTOGAs-MacBook-Pro-5.local>
2012-03-07 04:33:02 +0900

    transfer new music earth project, because I want to use story board and name of app "Music Earth"

--- Music Earth/Music_EarthViewController.h
+++ Music Earth/Music_EarthViewController.h
@@ -7,7 +7,58 @@
 //
 
 #import <UIKit/UIKit.h>
-
-@interface Music_EarthViewController : UIViewController
-
+#import <MediaPlayer/MediaPlayer.h>
+#import <MapKit/MapKit.h>
+#import <CoreLocation/CoreLocation.h>
+@interface Music_EarthViewController : UIViewController<MPMediaPickerControllerDelegate, MKMapViewDelegate, CLLocationManagerDelegate>{
+    //ipod
+    MPMusicPlayerController *player;
+    IBOutlet UIView *viewMainSide;
+    IBOutlet UILabel *labelSongTitle;
+    IBOutlet UILabel *labelAlbumTitle;
+    IBOutlet UILabel *labelArtist;
+    IBOutlet UIImageView *imageArtWork;
+    IBOutlet UISlider *sliderPlayerPos;
+    IBOutlet UILabel *labelPastTime;
+    IBOutlet UILabel *labelRestTime;
+    IBOutlet UIButton *buttonPlay;
+    IBOutlet UIButton *buttonPause;
+    //map
+    IBOutlet MKMapView *myMapView;
+    CLLocationManager *myLocationManager;
+    IBOutlet UILabel *latitude;
+    IBOutlet UILabel *longitude;
+    IBOutlet UIImageView *compassImg;
+    //time
+    NSTimer *timerClock;//for renew the time view
+    IBOutlet UILabel *labelDate;
+    IBOutlet UILabel *labelTime;
+    //volume
+    int volumeChagedTimes;
+    IBOutlet UILabel *labelVolumeChangedTimes;
+    IBOutlet UILabel *labelVolumeNum;
+    IBOutlet UISlider *sliderVolume;
+    //scene
+    IBOutlet UIButton *buttonScene1;
+    IBOutlet UIButton *buttonScene2;
+    IBOutlet UIButton *buttonScene1Back;
+    IBOutlet UIButton *buttonScene2Back;
+    
+    //xml
+    int sceneNum;
+    
+    
+}
+-(IBAction) playOrPause;
+-(IBAction) skipToNext;
+-(IBAction) skipToPrevious;
+-(IBAction) flipTOMoreSide;
+-(IBAction) flipToMainSide;
+-(IBAction) showMediaPicker;
+-(IBAction) volumeChange;
+-(IBAction) playerPosChange;
+-(IBAction) like;
+-(IBAction) dislike;
+-(IBAction) scene1;
+-(IBAction) scene2;
 @end
--- Music Earth/Music_EarthViewController.m
+++ Music Earth/Music_EarthViewController.m
@@ -22,7 +22,272 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    //ipod
+    player = [MPMusicPlayerController applicationMusicPlayer];
+    
+    MPMediaQuery *query = [MPMediaQuery artistsQuery];
+    [player setQueueWithQuery:query];
+    
+    
+    player.repeatMode = MPMusicRepeatModeAll;
+    player.shuffleMode = MPMusicShuffleModeOff;
+    [player play];
+    
+    [self updateView];
+    
+    //notification for itemChanged
+    NSNotificationCenter *myNotificationCenter = [NSNotificationCenter defaultCenter];
+    [myNotificationCenter 
+     addObserver:self 
+     selector:@selector(didItemChanged: ) 
+     name:MPMusicPlayerControllerNowPlayingItemDidChangeNotification 
+     object:player];
+    [player beginGeneratingPlaybackNotifications];
+    //notification for volume changed
+    [myNotificationCenter 
+     addObserver:self 
+     selector:@selector(didVolumeChanged: ) 
+     name:MPMusicPlayerControllerVolumeDidChangeNotification
+     object:player];
+    //player position
+    labelPastTime.text =[NSString stringWithFormat:@"%d", player.currentPlaybackTime];
+    //player volume
+    sliderVolume.value = player.volume;
+    labelVolumeNum.text = [NSString stringWithFormat:@"%f", sliderVolume.value];
+    //map
+    myLocationManager = [[CLLocationManager alloc] init];
+    myLocationManager.delegate = self;
+    myLocationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters;
+    myLocationManager.distanceFilter = kCLHeadingFilterNone;
+    [myLocationManager startUpdatingLocation];
+    [myLocationManager startUpdatingHeading];
+    
+    //time
+    timerClock=[NSTimer scheduledTimerWithTimeInterval:1.0f target:self selector:@selector(updateClock:) userInfo:nil repeats:YES];
+    
+    //read user defaults
+    //play button
+    buttonPlay.hidden = false;
+    buttonPause.hidden = true;
+    
+    //scene
+    [buttonScene1 setTitle:@"Relax" forState:UIControlStateNormal];
+    [buttonScene2 setTitle:@"Work" forState:UIControlStateNormal];
+    sceneNum=1;
 }
+
+//ipod----------------------------------------------------
+-(void)didItemChanged: (NSNotification*) aNote{
+    NSString *noteName = [aNote name];
+    if ([noteName isEqualToString:MPMusicPlayerControllerNowPlayingItemDidChangeNotification]) {
+        //update view
+        [self updateView];
+    }
+}
+
+//volume
+-(void)didVolumeChanged: (NSNotification*) aNote{
+    NSString *noteName = [aNote name];
+    if ([noteName isEqualToString:MPMusicPlayerControllerVolumeDidChangeNotification]) {
+        labelVolumeChangedTimes.text = [NSString stringWithFormat:@"%d", volumeChagedTimes++];
+        sliderVolume.value = player.volume;
+        
+    }
+}
+
+-(IBAction)playOrPause{
+    MPMusicPlaybackState state = player.playbackState;
+    if (state == MPMusicPlaybackStatePaused) {
+        buttonPlay.hidden = false;
+        buttonPause.hidden = true;
+        [player play];
+    }
+    else if(state == MPMusicPlaybackStatePlaying){
+        buttonPlay.hidden = true;
+        buttonPause.hidden = false;
+        [player pause];
+    }
+}
+
+-(IBAction)skipToNext{
+    [player skipToNextItem];
+}
+
+-(IBAction)skipToPrevious{
+    
+    if (player.currentPlaybackTime < 3.0) {
+        [player skipToPreviousItem];
+    }else{
+        [player skipToBeginning];
+    }
+    
+}
+
+-(IBAction)volumeChange{
+    labelVolumeNum.text = [NSString stringWithFormat:@"%f", sliderVolume.value];
+    player.volume = sliderVolume.value;
+}
+-(IBAction)playerPosChange{
+    player.currentPlaybackTime = sliderPlayerPos.value;
+    labelPastTime.text = [NSString stringWithFormat:@"%01i:%02i ", (int)sliderPlayerPos.value/60, (int)sliderPlayerPos.value%60];
+    labelRestTime.text = [NSString stringWithFormat:@"-%01i:%02i ", ((int)sliderPlayerPos.maximumValue-(int)sliderPlayerPos.value)/60, ((int)sliderPlayerPos.maximumValue-(int)sliderPlayerPos.value)%60];
+}
+
+-(IBAction)like{
+    
+    
+}
+
+-(IBAction)dislike{
+    [player skipToNextItem];
+}
+
+
+-(void)updateView{
+    MPMediaItem *curItem = [player nowPlayingItem];
+    labelSongTitle.text = [curItem valueForProperty:MPMediaItemPropertyTitle];
+    labelAlbumTitle.text = [curItem valueForProperty:MPMediaItemPropertyAlbumTitle];
+    labelArtist.text = [curItem valueForProperty:MPMediaItemPropertyArtist];
+    labelPastTime.text = [NSString stringWithFormat:@"%01i:%02i ", 0, 0];    
+    labelRestTime.text = [NSString stringWithFormat:@"-%01i:%02i ", ((int)sliderPlayerPos.maximumValue-(int)sliderPlayerPos.value)/60, ((int)sliderPlayerPos.maximumValue-(int)sliderPlayerPos.value)%60];
+    sliderPlayerPos.maximumValue = [[curItem valueForProperty:MPMediaItemPropertyPlaybackDuration] longValue];
+    sliderPlayerPos.minimumValue = 0;
+    sliderPlayerPos.value = player.currentPlaybackTime;    
+    MPMediaItemArtwork *artWork;
+    artWork =[curItem valueForProperty:MPMediaItemPropertyArtwork];
+    imageArtWork.image = [artWork imageWithSize:imageArtWork.frame.size];
+    //scene
+    switch (sceneNum) {
+        case 1:
+            buttonScene1Back.highlighted = true;
+            buttonScene2Back.highlighted = false;            
+            break;
+        case 2:
+            buttonScene1Back.highlighted = false;
+            buttonScene2Back.highlighted = true;
+        default:
+            break;
+    }
+    
+    //user default
+    NSNumber* sceneNSNum = [[NSNumber alloc] initWithInt:sceneNum];
+    NSMutableDictionary* pin = [NSMutableDictionary dictionaryWithObjectsAndKeys: 
+                                [curItem valueForProperty:MPMediaItemPropertyTitle], @"Title", 
+                                [curItem valueForProperty:MPMediaItemPropertyArtist], @"Artist",
+                                [curItem valueForProperty:MPMediaItemPropertyAlbumTitle], @"Album",
+                                latitude.text, @"Latitude",
+                                longitude.text, @"Longitude",
+                                labelDate.text, @"Time",
+                                labelVolumeNum.text, @"Volume",
+                                sceneNSNum, @"Scene",
+                                nil];
+    
+    NSUserDefaults* defaults = [NSUserDefaults standardUserDefaults];
+    NSArray *arrayOld=[defaults arrayForKey:@"userData"];
+    NSLog(@"arrayOldNum: %d", arrayOld.count);
+    
+    NSMutableArray* array = [NSMutableArray array];
+    //add old pins
+    for (int i=0; i<arrayOld.count; i++) {
+        [array addObject:[arrayOld objectAtIndex:i]];
+    }
+    //add new pin
+    [array addObject:pin];
+    //[array addObject:myArray];
+    
+    
+    
+    [defaults setObject:array forKey:@"userData"];
+    if ( ![defaults synchronize] ) {
+        NSLog( @"failed ..." );
+    }
+    //NSDictionary* hoge = [userDefaults dictionaryForKey:@"userData"];
+    NSLog(@"%d", array.count);
+    
+    
+}
+
+
+//picker----------------------------------------------------
+- (void) mediaPicker: (MPMediaPickerController *) mediaPicker
+   didPickMediaItems: (MPMediaItemCollection *) collection {
+    [self dismissModalViewControllerAnimated: YES];
+    //[self updatePlayerQueueWithMediaCollection: collection];
+    for(MPMediaItem *item in collection.items){;
+        NSLog(@"title: %@", [item valueForProperty:MPMediaItemPropertyTitle]);
+    };
+    [player setQueueWithItemCollection:collection];
+    [player play];
+}
+- (void) mediaPickerDidCancel: (MPMediaPickerController *) mediaPicker {
+    [self dismissModalViewControllerAnimated: YES];
+}
+
+- (IBAction) showMediaPicker{
+    
+    MPMediaPickerController *picker =
+    [[MPMediaPickerController alloc]
+     initWithMediaTypes: MPMediaTypeAnyAudio]; // 1
+    [picker setDelegate: self]; // 2
+    [picker setAllowsPickingMultipleItems: YES]; // 3
+    picker.prompt =
+    NSLocalizedString (@"Add songs to play",
+                       "Prompt in media item picker");
+    [self presentModalViewController: picker animated: YES]; // 4
+    [picker release];
+}
+//map----------------------------------------------------
+-(void) locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation{
+    latitude.text = [NSString stringWithFormat:@"%f", newLocation.coordinate.latitude];
+    longitude.text = [NSString stringWithFormat:@"%f", newLocation.coordinate.longitude];
+    
+    MKCoordinateRegion  region = myMapView.region;
+    region.center.latitude = newLocation.coordinate.latitude;
+    region.center.longitude = newLocation.coordinate.longitude;
+    region.span.latitudeDelta = 0.01;
+    region.span.longitudeDelta = 0.01;
+    [myMapView setRegion:region animated:YES];
+}
+
+-(void) locationManager:(CLLocationManager *)manager didUpdateHeading:(CLHeading *)newHeading{
+    compassImg.transform = CGAffineTransformMakeRotation(newHeading.magneticHeading * M_PI/180*-1);
+}
+
+
+//time------------------------------------------------------------------
+-(void) updateClock:(NSTimer*) theTimer{
+    NSDate* date = [NSDate date];
+    NSDateFormatter* form = [[NSDateFormatter alloc] init];
+    //date
+    [form setDateStyle:NSDateFormatterFullStyle];
+    [form setTimeStyle:NSDateFormatterNoStyle];
+    labelDate.text = [form stringFromDate: date];
+    //time
+    [form setDateStyle:NSDateFormatterNoStyle];
+    [form setTimeStyle:NSDateFormatterMediumStyle];
+    labelTime.text = [form stringFromDate: date];
+    
+    //ipod progress bar
+    sliderPlayerPos.value = sliderPlayerPos.value+1;
+    labelPastTime.text = [NSString stringWithFormat:@"%01i:%02i ", (int)sliderPlayerPos.value/60, (int)sliderPlayerPos.value%60];
+    labelRestTime.text = [NSString stringWithFormat:@"-%01i:%02i ", ((int)sliderPlayerPos.maximumValue-(int)sliderPlayerPos.value)/60, ((int)sliderPlayerPos.maximumValue-(int)sliderPlayerPos.value)%60];
+    
+}
+
+//scene----------------------------------------------------------------------------------
+-(IBAction)scene1{
+    buttonScene1Back.highlighted=true;
+    buttonScene2Back.highlighted=false;
+    sceneNum = 1;
+}
+-(IBAction)scene2{
+    buttonScene2Back.highlighted=true;
+    buttonScene1Back.highlighted=false;
+    sceneNum = 2;
+    
+}
+
 
 - (void)viewDidUnload
 {
